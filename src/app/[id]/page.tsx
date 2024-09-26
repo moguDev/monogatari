255958acfd8b00f38c21fc0818b868054279599a
moguDev <mail@sho-horiguchi.jp>
2024-09-26 17:02:21 +0900

    動的OGP

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -13,10 +13,10 @@ export async function generateMetadata({
       title: "モノがたり | 捨てられない「モノ」の使い道をみんなで考えるアプリ",
       description:
         "思い出が詰まっていて捨てられないけど、使い道に困っているモノはありませんか？「モノがたり」は、そんなモノの写真と、モノの思い出を語りみんなで新しい活用方法を考えるアプリケーションです。大切なモノに新しい物語を一緒に見つけましょう。",
-      url: `https://モノがたり.com/${id}`,
+      url: `https://xn--zckuap7azdvfzd.com/${id}`,
       images: [
         {
-          url: `https://モノがたり.com/${id}/opengraph-image`,
+          url: `https://xn--zckuap7azdvfzd.com/${id}/opengraph-image`,
           width: 1200,
           height: 630,
           alt: `${id}`,
@@ -29,7 +29,7 @@ export async function generateMetadata({
       title: "モノがたり | 捨てられない「モノ」の使い道をみんなで考えるアプリ",
       description:
         "思い出が詰まっていて捨てられないけど、使い道に困っているモノはありませんか？「モノがたり」は、そんなモノの写真と、モノの思い出を語りみんなで新しい活用方法を考えるアプリケーションです。大切なモノに新しい物語を一緒に見つけましょう。",
-      images: `https://モノがたり.com/${id}/opengraph-image`,
+      images: `https://xn--zckuap7azdvfzd.com/${id}/opengraph-image`,
     },
   };
 }
